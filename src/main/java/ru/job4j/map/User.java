5c562213e1cd0009685cc2926ce2a5716e5f4ff6
Elena <bond.elena.15.05@gmail.com>
2024-07-07 22:37:34 +0300

    5. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,9 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class User {
     private String name;
@@ -16,24 +13,27 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
     public static void main(String[] args) {
         Map<User, Object> map = new HashMap<>();
         User one = new User("Ivan", 10, new GregorianCalendar(1990, 1, 1));
         User two = new User("Ivan", 10, new GregorianCalendar(1990, 1, 1));
         map.put(one, new Object());
         map.put(two, new Object());
+        System.out.println(one.hashCode() == two.hashCode());
         System.out.println(map);
         /**
-         * 5.1 Пары попали в разные бакеты.
-         * 5.2 Проверка ключей на равенство их хеш кодов проводилась.
-         * Была использована реализация по умолчанию из класса Object
-         * (неявного родителя всех классов, в том числе и User)
-         * которая возвращает уникалный hash для каждого объекта основанный на адресе в памяти,
-         * а не на ключевых свойствах объекта.
-         * 5.3 Прооверка на equals() не проводилась.
-         * Потому что ключи попали в разные бакеты,
-         * а ключи из разных бакетов не сравниваются друг с другом,
-         * потому что между ними нет коллизии.
+         * 6.1 Пары попали в один бакет.
+         * 6.2 Проверка ключей на равенство хэш-кодов проводилась. Потому что пары попали в один бакет.
+         * 6.3 Проверка ключей на равенство через метод equals() тоже проводилась,
+         * потому что их хэш-коды равны.
+         * Т.к метод equals() не переопределён, то для ключей one и two он вернул false.
+         * В итоге в карте занят только один бакет в котором лежит Entry с первой добавленной парой,
+         * у которой есть ссылка на Entry со второй добавленной парой.
          */
     }
 }
