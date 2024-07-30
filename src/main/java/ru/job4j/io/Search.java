750452726cb7e396434d569129b9f93c06bf20db
Elena <bond.elena.15.05@gmail.com>
2024-07-30 16:58:16 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -7,15 +7,31 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * Точка входа ожидает два аргумента запуска: стартовая папка и расширение по которому будут отфильтрованы файлы.
+ */
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validArgs(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        if (args[0].isBlank() || args[1].isBlank()) {
+            throw new IllegalArgumentException("Arguments can not be empty.");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Not valid extension format. Extension must start with \".\"");
+        }
     }
 }
