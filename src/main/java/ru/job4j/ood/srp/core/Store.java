c06341c599ebf8b5b1c21c813f52c6ade91fef3d
Elena <bond.elena.15.05@gmail.com>
2024-12-03 21:38:34 +0300

    0. Принцип единственной ответственности [#4913 #524360]

---
+++ src/main/java/ru/job4j/ood/srp/core/Store.java
@@ -0,0 +1,25 @@
+package ru.job4j.ood.srp.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+
+    /**
+     * Проблема - создание объекта другого класса внутри текущего.
+     * Решене - свойство объекта инициализировать через конструктор и входящие параметры.
+      */
+    List<Toy> data = new ArrayList<>();
+    int size = 1;
+
+    /**
+     * Проблема - метод не только сохраняет но и создаёт сохраняемый объект.
+     * Решение - объект создавать извне и передавать его в качестве параметра в метод для сохранения.
+      */
+    public Toy add(String name) {
+        Toy toy = new Toy(name);
+        toy.setId(size++);
+        data.add(toy);
+        return toy;
+    }
+}
---
+++ src/main/java/ru/job4j/ood/srp/core/Toy.java
@@ -0,0 +1,27 @@
+package ru.job4j.ood.srp.core;
+
+public class Toy {
+
+    private int id;
+    private String name;
+
+    public Toy(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
