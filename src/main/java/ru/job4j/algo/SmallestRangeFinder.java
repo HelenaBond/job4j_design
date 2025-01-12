f99158bb337d0d76db2c6be14f5a9808527ef1bf
Elena <bond.elena.15.05@gmail.com>
2025-01-13 00:31:51 +0300

    Aлгоритм двух указателей [#505153]

---
+++ src/main/java/ru/job4j/algo/SmallestRangeFinder.java
@@ -0,0 +1,30 @@
+package ru.job4j.algo;
+
+import java.util.*;
+
+public class SmallestRangeFinder {
+
+    public static int[] findSmallestRange(int[] nums, int k) {
+        int left = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) {
+                left = i;
+            }
+            if (i - left == k - 1) {
+                return new int[] {left, i};
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 7, 9};
+        int k = 3;
+        int[] result = findSmallestRange(nums, k);
+        if (result != null) {
+            System.out.println("Наименьший диапазон с " + k + " различными элементами: " + Arrays.toString(result));
+        } else {
+            System.out.println("Такой диапазон не существует.");
+        }
+    }
+}
