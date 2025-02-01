fa33d4d00c3885cb084822fd587d03401e3aaaeb
Elena <bond.elena.15.05@gmail.com>
2025-02-01 08:33:50 +0300

    Вывод бинарного дерева на консоль [#504978]

---
+++ src/main/java/ru/job4j/algo/tree/visual/Main.java
@@ -0,0 +1,8 @@
+package ru.job4j.algo.tree.visual;
+
+public class Main {
+    public static void main(String[] args) {
+        ExampleTree exampleTree = new ExampleTree();
+        System.out.println(exampleTree);
+    }
+}
---
+++ src/main/java/ru/job4j/algo/tree/visual/VisualNode.java
@@ -0,0 +1,9 @@
+package ru.job4j.algo.tree.visual;
+
+public interface VisualNode {
+    VisualNode getLeft();
+
+    VisualNode getRight();
+
+    String getText();
+}
